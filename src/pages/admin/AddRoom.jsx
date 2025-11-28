2f85f417fc11c98f850f84169e3bc2bdb2497c92
anagha2307 <anaghavarghese100@gmail.com>
2025-11-29 01:16:13 +0530

    Frontend Admin Side

--- src/pages/admin/AddRoom.jsx
+++ src/pages/admin/AddRoom.jsx
@@ -1,17 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../Components/admin/Navbar'
 import AdminSideBar from '../../Components/admin/AdminSideBar'
 
 const AddRoom = () => {
+  const [images, setImages] = useState({
+    1: null,
+    2: null,
+    3: null,
+    4: null,
+  })
+  const [inputs, setInputs] = useState({
+    roomType: '',
+    price: 0,
+    amenities: {
+      'Free Wifi': 'false',
+      'Free Breakfast': 'false',
+      'Mountain View': 'false',
+      'Pool access': 'false',
+      'Room service': 'false',
+    }
+  })
   return (
     <>
-    <div className="flex flex-col h-screen">
+      <div className="flex flex-col h-screen">
         <Navbar />
         <div className="flex h-full">
           <AdminSideBar />
+
+          <form className="p-5 m-6">
+            <div>
+              <div className="flex flex-col items-start text-left">
+                <h1 className="text-3xl mt-5 md:text-4xl">Add Room</h1>
+                <p className="text-sm md:text-base text-gray-500/90 mt-2 max-w-174">
+                  Provide essential information to help guests find the right stay. A well-structured listing improves bookings.
+                </p>
+              </div>
+            </div>
+            {/* Image Upload */}
+            <p className="text-gray-800 my-6">Images</p>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6 sm:flex my-2">
+              {
+                Object.keys(images).map((key) => (
+                  <label htmlFor={`roomImages${key}`} key={key}>
+                    <img src={images[key] ? URL.createObjectURL(images[key]) : "https://cdn.pixabay.com/photo/2017/02/07/02/16/cloud-2044823_1280.png"} alt="upload fimage" className="max-h-13 cursor-pointer opacity-80" />
+                    <input type="file" accept='image/*' hidden id={`roomImages${key}`} onChange={e => setImages({ ...images, [key]: e.target.files[0] })} />
+                  </label>
+                ))
+              }
+            </div>
+            <div className="w-full flex max-sm:flex-col sm:gap-4 mt-4">
+              <div className="flexx-1 max-w-48">
+                <p className="text-gray-800 mt-4">Room Type</p>
+                <select onChange={e =>setInputs({...inputs,roomType:e.target.value}) } name="room" id="roomType" className="border border-gray-200 text-gray-600 opacity-70 mt-1 rounded p-2 w-full">
+                    <option value="">Select Room Type</option>
+                    <option value="">Single Bed</option>
+                    <option value="">Double Bed</option>
+                    <option value="">Luxury Room</option>
+                    <option value="">Family Suite</option>
+
+                </select>
+              </div>
+              <div className="flexx-1 max-w-48">
+                <p className="text-gray-800 mt-4">Price Per Night</p>
+                <input onChange={e =>setInputs({...inputs,price:e.target.value}) } value={inputs.price} className="border border-gray-200 text-gray-600 opacity-70 mt-1 rounded p-2 w-24"/>
+              </div>
+            </div>
+
+          </form>
         </div>
-    </div>
-      
+      </div >
+
     </>
   )
 }
--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -20,33 +20,62 @@ const Dashboard = () => {
               </p>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4 my-8 w-full">
-              <div className="bg-blue-100 border border-indigo-400/10 flex items-center rounded p-4 pr-8">
-                <FontAwesomeIcon icon={faBookJournalWhills} className="max-sm:hidden text-xl text-grey-800" />
+              <div className="bg-blue-100 border border-indigo-400/10 flex lg:flex-row md:flex-col items-center rounded md:p-4 p-2 md:gap-2 gap-4">
+                <FontAwesomeIcon icon={faBookJournalWhills} className="text-xl text-grey-800" />
                 <div className="flex flex-col sm:ml-4 font-medium">
-                  <p className="text-blue-500 text-2xl">Total Bookings</p>
-                  <p className="text-neutral-900 text-xl mx-8">40</p>
+                  <p className="text-blue-500 text-xl lg:text-2xl">Total Bookings</p>
+                  <p className="text-neutral-900 text-xl">40</p>
                 </div>
               </div>
-              <div className="bg-blue-100 border border-indigo-400/10 flex items-center rounded p-4 pr-8">
-                <FontAwesomeIcon icon={faSackDollar} className="max-sm:hidden text-xl text-grey-800" />
+              <div className="bg-blue-100 border border-indigo-400/10 flex lg:flex-row md:flex-col items-center rounded md:p-4 p-2 md:gap-2 gap-4">
+                <FontAwesomeIcon icon={faSackDollar} className="text-xl text-grey-800" />
                 <div className="flex flex-col sm:ml-4 font-medium">
-                  <p className="text-blue-500 text-2xl">Total Revenue</p>
-                  <p className="text-neutral-900 text-xl mx-8">$ 5098</p>
+                  <p className="text-blue-500 text-xl lg:text-2xl">Total Revenue</p>
+                  <p className="text-neutral-900 text-xl">$5098</p>
                 </div>
               </div>
-              <div className="bg-blue-100 border border-indigo-400/10 flex items-center rounded p-4 pr-8">
+              <div className="bg-blue-100 border border-indigo-400/10 flex lg:flex-row md:flex-col items-center rounded md:p-4 p-2 md:gap-2 gap-4">
                 <FontAwesomeIcon icon={faUsers} className="max-sm:hidden text-xl text-grey-800" />
                 <div className="flex flex-col sm:ml-4 font-medium">
-                  <p className="text-blue-500 text-2xl">Total Users</p>
-                  <p className="text-neutral-900 text-xl mx-8">35</p>
+                  <p className="text-blue-500 text-xl lg:text-2xl">Total Users</p>
+                  <p className="text-neutral-900 text-xl">35</p>
                 </div>
               </div>
             </div>
             <div className="mt-2">
-              h2.text
+              <h2 className="text-blue-950 text-xl font-medium mb-5">Recent Bookings</h2>
+              <div className="w-full max-w-3xl text-left border border-gray-200 rounded-lg">
+                  <table className="w-full text-center">
+                    <thead className="bg-gray-100">
+                      <tr>
+                        <th className="py-3 py-4 text-gray-800 font-medium">Username</th>
+                        <th className="py-3 py-4 text-gray-800 font-medium">Hotel name</th>
+                        <th className="py-3 py-4 text-gray-800 font-medium">Total amount</th>
+                        <th className="py-3 py-4 text-gray-800 font-medium">Payment Status</th>
+                      </tr>
+                    </thead>
+                    <tbody className="text-sm">
+                      <tr>
+                        <td className="py-3 py-4 text-gray-700 border-t border-gray-200">User</td>
+                        <td className="py-3 py-4 text-gray-700 border-t border-gray-200">Double Bed</td>
+                        <td className="py-3 py-4 text-gray-700 border-t border-gray-200">$ 400</td>
+                        <td className="py-3 py-4 text-gray-700 border-t border-gray-200">
+                          <button className="px-3 py-1 text-xs rounded-full mx-auto bg-green-200 text-green-600">Completed</button>
+                        </td>   
+                      </tr>
+                      <tr>
+                        <td className="py-3 py-4 text-gray-700 border-t border-gray-200">User</td>
+                        <td className="py-3 py-4 text-gray-700 border-t border-gray-200">Double Bed</td>
+                        <td className="py-3 py-4 text-gray-700 border-t border-gray-200">$ 400</td>
+                        <td className="py-3 py-4 text-gray-700 border-t border-gray-200">
+                          <button className="px-3 py-1 text-xs rounded-full mx-auto bg-yellow-200 text-red-600">Pending</button>
+                        </td>   
+                      </tr>
+                    </tbody>
+                  </table>
+              </div>
             </div>
           </div>
-
         </div>
       </div>
     </>
